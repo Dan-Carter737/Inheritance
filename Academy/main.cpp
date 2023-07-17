ca90b4e79f5c436fe8f72bbba8a1f1a9e2e9a831
Денис Коротков <Dan-Carter@mail.ru>
2023-07-17 18:30:29 +0400

    Add project files.

---
+++ Academy/main.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+
+class Human {
+
+protected:
+	string last_name;
+	string first_name;
+	int age;
+
+public:
+	const std::string& get_last_name()const
+	{
+		return last_name;
+	}
+	const std::string& get_first_name()const
+	{
+		return first_name;
+	}
+	int get_age()const {
+
+		return age;
+	}
+
+	void set_last_name(const std::string& last_name) {
+
+		this->last_name = last_name;
+	}
+	void set_first_name(const std::string& first_name) {
+
+		this->first_name = first_name;
+	}
+	void set_age(int age) {
+
+		this->age = age;
+	}
+
+	Human(const std::string& last_name, const std::string& first_name, int age) {
+
+		set_last_name(last_name);
+		set_first_name(first_name);
+		set_age(age);
+		cout << "Hconstructor\t" << this << endl;
+	}
+	~Human() 
+	{
+		cout << "Destructor\t" << this << endl;
+	}
+	void print() const
+	{
+		cout << last_name << " " << first_name << " " << age << endl;
+	}
+
+
+};
+
+class Student :public Human
+{
+
+	std::string speciality;
+	std::string group;
+	double rating;
+	double attendance;
+
+public:
+	const std::string& get_speciality()const
+	{
+		return speciality;
+
+	}
+	const std::string& get_group()const
+	{
+		return group;
+	}
+	double get_rating() const
+	{
+		return rating;
+	}
+	double get_attendance() const
+	{
+		return attendance;
+	}
+	void set_speciality(const std::string& speciality)
+	{
+		this->speciality = speciality;
+	}
+	void set_group(const std::string& group)
+	{
+		this->group = group;
+	}
+	void set_rating(double rating)
+	{
+		this->rating = rating;
+	}
+	void set_attendance(double attendance)
+	{
+		this->attendance = attendance;
+	}
+	//Constructors:
+
+	Student(const std::string& last_name, const std::string& first_name, int age,
+		const std::string& speciality, const std::string& group, double rating, double attendance
+	) :Human(last_name, first_name, age)
+	{
+		set_speciality(speciality);
+		set_group(group);
+		set_rating(rating);
+		set_attendance(attendance);
+		cout << "Sconstructor\t" << this << endl;
+	}
+
+	void print() const
+	{
+		Human::print();
+		cout << speciality << " " << group << " " << rating << " " << attendance << endl;
+	}
+
+
+};
+
+
+
+void main() {
+
+	setlocale(LC_ALL, "");
+	Human human("Montana", "Antonio", 30);
+	human.print();
+
+
+	Student stud("Pinkman", "Jessie", 25, "Chemistry", "WW_220", 95, 98);
+	stud.print();
+
+
+
+}
